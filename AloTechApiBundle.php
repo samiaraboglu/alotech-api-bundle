b3919e8b348d8e2948b260ab248518527924d2ce
Sami Araboğlu <samiaraboglu@gmail.com>
2019-03-05 13:23:40 +0300

    Initial commit

---
+++ AloTechApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Samiax\AloTechApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AloTechApiBundle extends Bundle
+{
+}
---
+++ DependencyInjection/SamiaxAloTechApiExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Samiax\AloTechApiBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ */
+class SamiaxAloTechApiExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('samiax_alotech_api.username', $config['username']);
+        $container->setParameter('samiax_alotech_api.app_token', $config['app_token']);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
